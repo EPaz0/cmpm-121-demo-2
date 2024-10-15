e01af1d6c06c8fd2f39207c782afeeb3e90ccc49
EPaz0 <enrique_pazg@yahoo.com>
2024-10-15 11:13:50 -0700

    Step 1 Complete Added Canvas and changed title

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,24 @@
 import "./style.css";
 
-const APP_NAME = "Hello Test";
+const APP_NAME = "Sketch";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+
+//Create h1 element and set app name
+const titleElement = document.createElement("h1");
+titleElement.textContent = APP_NAME;
+
+app.appendChild(titleElement);
+
+
+//Create canvas element
+const canvasElement  = document.createElement("canvas");
+canvasElement.width = 256;
+canvasElement.height = 256;
+
+
+
+
+// Append the canvas element to the app container
+app.appendChild(canvasElement);
